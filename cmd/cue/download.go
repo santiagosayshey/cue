ff6de1ad8c30385a2cc61f1a8a145bf5a408d96c
Sam Chau <schau22@pm.me>
2026-07-04 01:44:19 +0930

    feat: downloader package

---
+++ cmd/cue/download.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"io"
+	"net/http"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+)
+
+type Downloader interface {
+	Download(url string, destPath string) error
+}
+
+type Youtube struct{}
+type GDrive struct{}
+
+func (yt Youtube) Download(url string, destPath string) error {
+	cmd := exec.Command("yt-dlp", "-x", "--audio-format", "mp3", "-o", "theme.mp3", url)
+	cmd.Dir = destPath
+	return cmd.Run()
+}
+
+func (gd GDrive) Download(url string, destPath string) error {
+	resp, err := http.Get("https://drive.google.com/uc?export=download&id=" + strings.Split(url, "/")[5])
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	file, err := os.Create(filepath.Join(destPath, "theme.mp3"))
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	_, err = io.Copy(file, resp.Body)
+	return err
+}
+
+func newDownloaders() map[string]Downloader {
+	return map[string]Downloader{
+		"youtube": Youtube{},
+		"gdrive":  GDrive{},
+	}
+}
